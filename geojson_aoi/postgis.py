26b524777a8d6b72750985445679a1e1cdefae27
Luke Everhart <luke.f.everhart@gmail.com>
2025-03-24 21:51:53 -0400

    Add test case for MultiPolygon with overlapping polygons

--- geojson_aoi/postgis.py
+++ geojson_aoi/postgis.py
@@ -130,8 +130,15 @@ class Normalize:
 
             SELECT * FROM merge_disjoints();
         """
+        
+        val2 = f"""
+            SELECT * FROM {table_id} a
+            INNER JOIN {table_id} b ON
+                (a.geometry && b.geometry AND ST_Overlaps(a.geometry, b.geometry))
+            WHERE a.id != b.id;
+        """
 
-        return val
+        return val2
     
 class PostGis:
     """A synchronous database connection.
