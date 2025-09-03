1e8d035243d33d03b4802d5cc400dfcd899f3c25
Luke Everhart <luke.f.everhart@gmail.com>
2025-09-03 19:19:37 -0400

    Mark test case async and add await

--- tests/_async/test_parser.py
+++ tests/_async/test_parser.py
@@ -4,6 +4,7 @@ import json
 import warnings
 
 import pytest
+import pytest_asyncio
 
 from geojson_aoi._async.parser import parse_aoi_async
 
@@ -17,11 +18,12 @@ def is_featcol_nested_polygon(geojson) -> bool:
     return False
 
 
+@pytest.mark.asyncio
 async def test_polygon(db, polygon_geojson):
     """A single Polygon."""
-    result = parse_aoi_async(db, polygon_geojson)
+    result = await parse_aoi_async(db, polygon_geojson)
     assert is_featcol_nested_polygon(result)
-    assert len((await result)["features"]) == 1
+    assert len(result["features"]) == 1
 
 
 def test_polygon_with_holes(db, polygon_holes_geojson):
