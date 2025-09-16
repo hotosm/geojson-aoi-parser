5e3d409534c16899530409a74a1d953aab7bd278
Luke Everhart <luke.f.everhart@gmail.com>
2025-09-15 21:25:08 -0400

    Only import pytest. Remove test

--- tests/_async/test_parser.py
+++ tests/_async/test_parser.py
@@ -3,14 +3,9 @@
 import json
 import warnings
 
-import pytest_asyncio
+import pytest
 
 from geojson_aoi._async.parser import parse_aoi_async
-
-
-@pytest_asyncio.fixture
-async def async_test():
-    yield "out"
 
 
 def is_featcol_nested_polygon(geojson) -> bool:
